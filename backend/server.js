de0831a1f96ff8f88983aec4a099be9f76cb2fb6
HuzaifaAslam99 <huzaifaaslam322@gmail.com>
2026-04-27 15:22:57 -0400

    small update

--- backend/server.js
+++ backend/server.js
@@ -23,9 +23,20 @@ app.use(express.json());
 
 // console.log("My DB URL is: ", process.env.TICKET_BOOKING_DB_URI);
 
-mongoose.connect(process.env.TICKET_BOOKING_DB_URI)
-  .then(() => console.log('Successfully connected to Ticket Booking Database'))
+const uri = process.env.TICKET_BOOKING_DB_URI;
+
+if (!uri) {
+    console.error("ERROR: TICKET_BOOKING_DB_URI is not defined in Environment Variables!");
+}
+
+mongoose.connect(uri)
+  .then(() => console.log('Successfully connected to MongoDB'))
   .catch(err => console.error('Database connection error:', err));
+
+
+// mongoose.connect(process.env.TICKET_BOOKING_DB_URI)
+//   .then(() => console.log('Successfully connected to Ticket Booking Database'))
+//   .catch(err => console.error('Database connection error:', err));
 
 
 app.get("/", (req, res) => {
